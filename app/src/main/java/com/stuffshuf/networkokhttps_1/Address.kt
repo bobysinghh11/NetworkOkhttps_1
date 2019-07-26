4f92fc153ab281450c84a61ff0ff25e3ccce5663
bobbysingh <Meghsingh@12345>
2019-07-26 08:13:37 +0530

    Initial commit

---
+++ app/src/main/java/com/stuffshuf/networkokhttps_1/Address.kt
@@ -0,0 +1,6 @@
+package com.stuffshuf.networkokhttps_1
+
+data class Address(
+    val city:String,
+    val street:String
+)
---
+++ app/src/main/java/com/stuffshuf/networkokhttps_1/User.kt
@@ -0,0 +1,10 @@
+package com.stuffshuf.networkokhttps_1
+
+
+    data class User(
+        val id:Int,
+        val name:String,
+        val username:String,
+        val email:String,
+        val address:Address
+    )
